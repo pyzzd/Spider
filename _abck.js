4cb5a91c588420214823d3055280f0be11c69312
Djj2391 <65949377+Djj2391@users.noreply.github.com>
2022-08-25 10:57:45 +0800

    阿卡迈1.75
    阿卡迈1.75sensor_data

---
+++ _abck.js
@@ -0,0 +1,168 @@
+var _acxj = ["csh", "RealPlayer Version Plugin", "then", "hku", "opera", "getStorageUpdates", "mn_poll", "firstLoad", "isBrave", "hbCalc", "position: relative; left: -9999px; visibility: hidden; display: block !important", "-1,2,-94,-121,", "1", ";", "DeviceOrientationEvent", "toElement", "_setBm", "pd", "hostname", "charAt", "-1,2,-94,-80,", "documentMode", "XPathResult", "round", "magnetometer", "enAddHidden", "name", "onmousedown", "ArialHebrew-Light", "pointerType", "Minion Pro", "ats", "Widevine Content Decryption Module", "-1,2,-94,-119,", "which", "XMLHttpRequest", "touchmove", "enReadDocUrl", "tme_cnt", "aj_type", "webdriver", "gf", "-1,2,-94,-103,", "hpd", "brand", "iPhotoPhotocast", "2d", "catch", "innerHeight", "pluginInfo", "x11:", "Shockwave for Director", "availHeight", "Waseem", "addEventListener", "innerWidth", "y1", "tduce_cnt_lmt", "credentials", "__webdriver_script_function", "emit", "Noto", "Comic Neue", "mn_psn", "ssh", "QuickTime Plug-in", "tact", "-1,2,-94,-110,", "readyState", "visibilitychange", "", "onload", "width", "check_ipr_signals", "abs", "mousemove", "z1", "vibrate", "sed", "devicePixelRatio", "open", "y", "POST", "ckie", ",uaend,", "apply", "t_dis", "bmisc", "rgb(120, 186, 176)", "tel", "sans-serif", "mn_rts", "Default Browser Helper", "Microsoft Sans Serif", "aj_indx", "div", "GET", "background-sync", "rCFP", "/_bm/_data", "gd", "wen", "rve", "pageX", "DeviceMotionEvent", "userAgentData", "runFonts", "selenium", "-1,2,-94,-126,", "gamma", "cookie_chk_read", ",s3:", "$chrome_asyncScriptInfo", "loap", "me_cnt", "getElementsByTagName", "activeElement", "mn_lc", "cdc_adoQpoasnfa76pfcZLmcfl_Promise", "$cdc_asdjflasutopfhvcZLmcfl_", "fmget_targets", "random", "aj_indx_doact", "innerHTML", "Microsoft Tai Le", "vcact", "non:", "vc", "mact", "-1,2,-94,-70,", "-1,2,-94,-118,", "pe_cnt", "Ubuntu Regular", "default_session", "clientX", "exception", "fsp", "ke_cnt", "Native Client", "-1,2,-94,-122,", "||", "==", "fc:", "clipboard-write", "fillText", "x", "plen", "hardwareConcurrency", "Mozilla/5.0 (", "get_heartbeat_timestamp", "sqrt", "isIgn", "sessionStorage", "bd", "vc_cnt_lmt", "Shree Devanagari 714", "acos", "onmouseup", "none", "aj_lmt_dmact", "mme_cnt_lmt", "hn", "MAX_VALUE", "timezoneOffsetKey", "prompt", "-1", "get_cf_date", ",cpen:", "colorDepth", "pact", "opc:", "mozvisibilitychange", "mozPhoneNumberService", "webkitRTCPeerConnection", "\\\\\"", "dm_en", "hasOwnProperty", "{\"sensor_data\":\"", "-1,2,-94,-127,", "mn_cd", "aj_ss", "cache", "join", "indexOf", "Constructor", "localStorageKey", "-1,2,-94,-116,", "strokeStyle", "touchend", "getdurl", "mn_get_current_challenges", "Cantarell", "event", "Nimbus Roman No 9 L", "rotationRate", "wv", "Menlo", "shiftKey", "granted", "mn_pr", "documentElement", "get_browser", "device-info", "UNMASKED_VENDOR_WEBGL", "do_dis", "ke_cnt_lmt", "unk", "fpcf", "devicemotion", "pme_cnt", "-1,2,-94,-131,", "od", "mn_get_new_challenge_params", "Version/4.0", "height", "params_url", "kact", "text", "button", "hf", "pointerup", "sd_debug", "stroke", "spynner_additional_js_loaded", "do_en", "Chrome Remote Desktop Viewer", "Adobe Acrobat", "abcdefhijklmnopqrstuvxyz1234567890;+-.", "dm:", "mn_ld", "span", "mn_wt", "getParameter", "charCode", "offsetWidth", "msManipulationViewsEnabled", "UNMASKED_RENDERER_WEBGL", "sensor_data", "PLUGINS", "getGamepads", "altKey", " Mobile", "enGetLoc", "mn_lcl", "bc", "getFloatVal", "loc", "screen", "geolocation", "pdet", "getVoices", "webkitHidden", "shift", "to", "dis", "voiceURI", "email", "Lobster", "type", "gbrv", "m,Ev!xV67BaU> eh2m<f3AG3@", "keypress", "location", "style", "rst", "src", "90px", "ambient-light-sensor", "TI-Nspire", "fm", "Google Chrome", "ir", "start_ts", "webstore", "-1,2,-94,-102,", "onpointerdown", "isTrusted", "denied", "__nightmare", "mn_w", "fpVal", "prod", "3", "New York", "mn_il", "vib:", "cal_dis", "startdoadma", "serviceWorker", "FileReader", "Candara", "parseInt", "cf_url", "calledSelenium", "cssText", "https://", "required", "mn_lg", "webkitTemporaryStorage", "accelerationIncludingGravity", "fas", "pageY", "_setFsp", "AdobeAAMDetect", "map", "mn_mc_indx", "query", "htc", "font", "Serif", "http://", "wl", "stack", "mozHidden", ",loc:", "get_mn_params_from_abck", "keyCode", "afSbep8yjnZUjq3aL010jO15Sawj2VZfdYK8uY90uxq", "cdc_adoQpoasnfa76pfcZLmcfl_Symbol", "Buffer", "chknull", "-1,2,-94,-106,", "calledPhantom", "touchstart", "webkitvisibilitychange", "childNodes", "monospace", "state", "ff", "accessibility-events", "hkp", "mn_mc_lmt", "x12:", "script", "lvc", ",s2:", "Avenir", "Quicksand", "applyFunc", "mn_tout", "mozIsLocallyAvailable", "cns", "mn_init", "-1,2,-94,-109,", "mme_cnt", "<init/>", "__webdriverFuncgeb", "hasIndexedDB", "cta", "metaKey", "split", "geb", "doa_throttle", ",it0", "mn_update_challenge_details", "Source Sans Pro", "getSupportedExtensions", " Safari/537.36", "psub", ",s1:", "gyroscope", "-1,2,-94,-117,", "Bell MT", "Ubuntu Medium", "pixelDepth", "iPad;", "weh", "d3", "protocol", "Wingdings 2", "exp", "cc", "__webdriver__chr", "mouse", "hvc", "RealPlayer(tm) G2 LiveConnect-Enabled Plug-In (32-bit)", "mn_rt", "iPhone", "x2", "localStorage", "t_tst", "-1,2,-94,-111,", "-1,2,-94,-115,", "{\"session_id\" : \"", "Silverlight Plug-In", "acceleration", "__selenium_unwrapped", "; ", "indexedDbKey", "clearCache", "Bodoni 72", "_Selenium_IDE_Recorder", "mn_s", "getContext", "Adobe Hebrew", "__driver_unwrapped", "Century Gothic", "-1,2,-94,-114,", "Futura", "mozConnection", "}", "URL", "serif", "ActiveXObject", "removeChild", "_abck", "plugins", "cka", "replace", "arc", "floor", "init_time", "Open Sans", "cs", ":", "Geneva", "<setSDFN>", "-1,2,-94,-120,", "Content-type", "Windows Media Player Plug-in Dynamic Link Library", "192px", "hypot", "mduce_cnt_lmt", "dme_vel", "fonts", "bind", "Lucida Sans", "hbs", "push", "hkd", "b", "x1", "storage", "document", "input", "hmd", "fontSize", "__lastWatirConfirm", "-", "cookie", "WebKit-integrierte PDF", "mouseup", "MSIE", "Papyrus", "pow", "e", "doact", "__webdriver_script_fn", "pua", "sdfn", "ITC Bodoni 72 Bold", "__webdriver_script_func", "cpa", "getElementsByName", "aj_indx_tact", "alpha", "doe_cnt_lmt", "chrome", "\\'", "informinfo", "pi", "mozRTCPeerConnection", "uar", "encode_utf8", "__fxdriver_unwrapped", "id", "microphone", "bat:", "msvisibilitychange", "hasLocalStorage", "appVersion", "parse", "cwen:", "get_stop_signals", "webgl", "n", "pme_cnt_lmt", "pointerdown", "onreadystatechange", "getAttribute", "clientY", "Lato", "mn_abck", "get_cookie", "hc", "jrs", "listFunctions", "pduce_cnt", "undef", "doNotTrack", ",\"auth\" : \"", "fpValstr", "spawn", "-1,2,-94,-124,", "onLine", "pe_vel", "defaultValue", "ipr", "Unity Player", "toFixed", "hte", "Basic ", "fillRect", "mousedown", "disFpCalOnTimeout", "permissions", "htm", "</setSDFN>", "t_en", "d2", "display", "apicall_bm", "-1,2,-94,-100,", "dme_cnt_lmt", "toLowerCase", "rir", "set_cookie", "attachEvent", "toString", "Press Start 2P", "Google Talk Plugin Video Renderer", "Mozilla Default Plug-in", "td", "payment-handler", "bm", "mmmmmmmmlli", "slice", "Palatino-Bold", "Google Earth Plug-in", "hmu", "hpu", "beta", "pstate", "_setPowState", "mn_state", "\",\"sensor_data\" : \"", "i1:", "dme_cnt", "Adobe Braille", "mediaDevices", "Batang", "te_vel", "forEach", "platformVersion", "XDomainRequest", "0", "availWidth", "call", "RTCPeerConnection", "__webdriver_evaluate", "fonts_optm", "Shockwave Flash", "send", "mozInnerScreenY", "YouTube Plug-in", "n_ck", "/", "mn_tcl", "bitness", ",", "build_pua", "Monospace", "function", "application/json", "Palatino", "Chrome PDF Viewer", "altFonts", "get_type", "Calibri", "parse_gp", "getHighEntropyValues", "den", "clipboard-read", "imul", "nav_perm", "hidden", "hts", "fromCharCode", "outerWidth", "xagg", "cdoa", "Apple Gothic", "Century", "fillStyle", "dm_dis", "session_id", "brv", "length", "-1,2,-94,-112,", "=", "Birch Std", "watinExpressionError", " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/", "-1,2,-94,-123,", "keyup", "getElementById", "wrc:", "onkeypress", "isc:", "domAutomationController", "onkeydown", "PointerEvent", "-1,2,-94,-105,", "value", "getStateField", "np", "mn_r", "SharePoint Browser Plug-in", "auth", "hasSessionStorage", "storeWebWideTrackingException", "touchcancel", "search", "navigator", "prototype", "mn_al", "Apple LiGothic", ",setSDFN:", ")", "brands", "webrtcKey", "OSMJIF", "accelerometer", "mduce_cnt", "now", "tduce_cnt", "onblur", "sc:", "dmact", "getTimezoneOffset", "currentScript", "ta", "ab", "doe_vel", "mr", "awesomium", "msHidden", "default", "sessionStorageKey", "Authorization", "Oswald", ",0", "rgb(102, 204, 0)", "keydown", "bdm", "onfocus", "Cambria", "Courier New", "body", "ctrlKey", "onpointerup", "dma_throttle", "__phantomas", "userAgent", "rVal", "Edge PDF Viewer", "__lastWatirPrompt", "_", "Fantasque Sans Mono", "aj_lmt_tact", "https:", "Mac OS X 10_5", "0a46G5m17Vrp4o4c", "<bpd>", "canvas", "2", "ke_vel", "bm-telemetry", "uaFullVersion", "16pt Arial", "te_cnt", "__selenium_evaluate", "#f60", "setRequestHeader", "bluetooth", "clientHeight", "fmz", "wr", "z", "product", "midi", "ver", "pduce_cnt_lmt", "Monaco", ",s7:", "__lastWatirAlert", "startTracking", "appMinorVersion", "onclick", "clientWidth", "me_vel", "createElement", "withCredentials", "Helvetica Neue", "\"}", "brave", "fpValCalculated", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/", "ce_js_post", "autocomplete", "is not a valid enum value of type PermissionName", "\"", "Microsoft.XMLHTTP", "<@nv45. F1n63r,Pr1n71n6!", "message", "Corsiva Hebrew", "-1,2,-94,-108,", "patp", "cdma", "requestMediaKeySystemAccess", "__driver_evaluate", "mn_sen", "Java Applet Plug-in", "onkeyup", "_setAu", "fidcnt", "WebEx64 General Plugin Container", ",mn_w:", "getBattery", "fmh", "offsetHeight", "undefined", "mn_stout", "doadma_en", "javaEnabled", "aeiouy13579", "click", "check_stop_protocol", "__fxdriver_evaluate", "Avenir Next", "js_post", "getExtension", "; path=/; expires=Fri, 01 Feb 2025 08:00:00 GMT;", "rotate_right", "callPhantom", "o9", "vc_cnt", "/get_params", "Times", "splice", "cookieEnabled", "__webdriver_unwrapped", "api_public_key", "_setIpr", "</bpd>", "sf4", "cbrt", "onmousemove", "calc_fp", "platform", "Droid Serif", "HTMLElement", "-1,2,-94,-101,", "language", "clipboard", "camera", "Gill Sans MT", "gb", "_phantom", "Java Plug-in 2 for NPAPI Browsers", "charCodeAt", "mn_ct", "mn_ts", "model", "string", "password", "ceil", "sort", "cdc_adoQpoasnfa76pfcZLmcfl_Array", "TouchEvent", "Microsoft Office Live Plug-in", "6pt Arial", "persistent-storage", "aj_lmt_doact", "Courier", "some", "speaker", "WEBGL_debug_renderer_info", "domAutomation", "mobile", "doe_cnt", "isChrome", "encode", "indexedDB", "data", "prevfid", "__$webdriverAsyncExecutor", "lastIndexOf", "stringify", "watinExpressionResult", "hmm", "appendChild", "fontFamily", "registerProtocolHandler", "webkitGetGamepads", "architecture", "driver", "requestWakeLock", "-1,2,-94,-129,", "PI", "ins", "wgl", "lang", "aj_indx_dmact", "hb", "pen", "all", "deviceorientation", "AlNile", "substring", "speechSynthesis", "on", "Roboto", "sendBeacon", "tme_cnt_lmt", "url", "getforminfo", "cma", "//", "dcs", "~", "notifications", "updatet", "atanh", "tst", "mozAlarms", "mn_cc", "off", "bpd", "gwd", "toDataURL", "number", "callSelenium", "productSub", "Damascus", "asin", "target"];
+var start_ts = Date.now ? Date.now() : +new Date
+var z1 = pi(start_ts / (2016 * 2016));
+var ttt = x2() % 1e7;
+function rir(t, a, e, n) {
+        return t > a && t <= e && (t += n % (e - a)) > e && (t = t - e + a),
+        t
+    }
+function od(t, a) {
+    try {
+        t = String(t);
+        a = String(a);
+        var e = []
+            , n = a.length;
+        if (n > 0) {
+            for (var o = 0; o < t.length; o++) {
+                var m = t.charCodeAt(o)
+                    , r = t.charAt(o)
+                    , i = a.charCodeAt(o % n);
+                m = rir(m, 47, 57, i),
+                m != t.charCodeAt(o) && (r = String.fromCharCode(m)),
+                    e["push"](r)
+            }
+            if (e.length > 0)
+                return e.join("")
+        }
+    } catch (t) {
+    }
+    return t
+}
+
+function get_cf_date() {
+    return Date.now ? Date.now() : +new Date
+}
+
+function uar() {
+    return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+}
+
+function ab(t) {
+    for (var a = 0, e = 0; e < t.length; e++) {
+            var n = t.charCodeAt(e);
+            n < 128 && (a += n)
+        }
+        return a
+}
+
+function pi(t) {
+    return parseInt(t)
+}
+
+function ff(t) {
+    return String[_acxj[577]](t)
+}
+
+function x2() {
+    var t = ff
+        , a = t(98) + t(109) + t(97) + t(107)
+        , e = t(103) + t(101) + t(116) + t(95) + t(99) + t(102) + t(95) + t(100) + t(97) + t(116) + t(101)
+        , n = get_cf_date
+        , o = 0;
+    return "function" == typeof n && (o = n()),
+        o
+}
+
+function gd() {
+    var t = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+        , a = "" + ab(t)
+        , e = start_ts / 2
+        , n = -1
+        , o = -1
+        , m = -1
+        , r = -1
+        , i = -1
+        , c = -1
+        , b = -1;
+
+    n = 1536
+    o = 824
+    m = 1536
+    r = 864
+    i = 722
+    c = 1536
+    b = 1536
+
+    var d3 = ttt
+    var d = Math.random()
+    var s = pi(1e3 * d / 2)
+    var k = d + "";
+
+    return k = k.slice(0, 11) + s,
+    t + ",uaend," + 12147 + "," + "20030107" + "," + "zh-CN" + "," + "Gecko" + "," + 5 + "," + 0 + "," + 0 + "," + 0 + "," + z1 + "," + d3 + "," + n + "," + o + "," + m + "," + r + "," + c + "," + i + "," + b + "," + ",cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1" + "," + a + "," + k + "," + e + "," + 0 + ",loc:" + ""
+}
+
+function cal_dis(t) {
+    var a = t[0] - t[1]
+        , e = t[2] - t[3]
+        , n = t[4] - t[5]
+        , o = Math.sqrt(a * a + e * e + n * n);
+    return Math.floor(o)
+}
+
+function jrs(t) {
+    for (var a = Math.floor(1e5 * Math.random() + 1e4), e = String(t * a), n = 0, o = [], m = e.length >= 18; o.length < 6;)
+        o["push"](parseInt(e["slice"](n, n + 2))),
+            n = m ? n + 3 : n + 2;
+    return [a, cal_dis(o)]
+}
+
+function cc(t) {
+    var a = t % 4;
+    2 == a && (a = 3);
+    var e = 42 + a
+        , n = function (t, a) {
+        return 0
+    };
+    if (42 == e)
+        var n = function (t, a) {
+            return t * a
+        };
+    else if (43 == e)
+        var n = function (t, a) {
+            return t + a
+        };
+    else
+        var n = function (t, a) {
+            return t - a
+        };
+    return n
+}
+function to() {
+        t = ttt
+        d3 = t;
+        for (var a = t, e = pi(ff(51)), n = 0; n < 5; n++) {
+            var o = pi(t / Math.pow(10, n)) % 10
+              , m = o + 1;
+            a = cc(o)(a, m)
+        }
+        o9 = a * e
+    return o9
+    }
+function load_sensor_data(url,e_) {
+    var ver = 1.75
+    var n_ = gd()
+    var _ = get_cf_date() - start_ts
+    var u = jrs(start_ts)
+    d2 = pi(z1 / 23);
+    f_ = pi(d2 / 6)
+    o9 = to()
+    g = [1, 32, 32, 0, 0, 0, 0, 0, 0, start_ts, -999999, d2, 0, 0, f_, 0, 0, _, 0, "0", e_, ab(e_), "-1", "-1", 30261693, "PiZtE", u[0], u[1], 0, -1]
+
+        var w = g.join(",")
+    sensor_data = ver + "-1,2,-94,-100," + n_ + "-1,2,-94,-131," + "" + "-1,2,-94,-101," + "do_en,dm_en,t_en" + "-1,2,-94,-105," + "0,0,0,0,2054,864,0;1,0,0,0,2073,883,0;" + "-1,2,-94,-102," + "0,0,0,0,2054,864,0;1,0,0,0,2073,883,0" + "-1,2,-94,-108," + "" + "-1,2,-94,-110," + "" + "-1,2,-94,-117," + "" + "-1,2,-94,-111," + "" + "-1,2,-94,-109," + "" + "-1,2,-94,-114," + "" + "-1,2,-94,-103," + "" + "-1,2,-94,-112," + url + "-1,2,-94,-115," + w + "-1,2,-94,-106," + "0,0"
+    sensor_data = sensor_data + "-1,2,-94,-119," + "-1" + "-1,2,-94,-122," + "0,0,0,0,1,0,0" + "-1,2,-94,-123," + "" + "-1,2,-94,-124," + "" + "-1,2,-94,-126," + "" + "-1,2,-94,-127," + 8;
+    var P = 24 ^ ab(sensor_data);
+    sensor_data = sensor_data + "-1,2,-94,-70," + "-1" + "-1,2,-94,-80," + "94" + "-1,2,-94,-116," + o9 + "-1,2,-94,-118," + P + "-1,2,-94,-129," + "" + "-1,2,-94,-121,"
+    return sensor_data
+}
+
+function get_sensor_data(l) {
+    url = l[0]
+    e_ = l[1]
+    var F = od("0a46G5m17Vrp4o4c", "afSbep8yjnZUjq3aL010jO15Sawj2VZfdYK8uY90uxq")["slice"](0, 16)
+    var D = Math.floor(get_cf_date() / 36e5)
+    var R = "7a74G7m23Vrp0o5c" + od(D, F) + load_sensor_data(url,e_)
+    sensor_data = R + ";" + 6 + ";" + -1 + ";" + 0
+    return sensor_data
+}
